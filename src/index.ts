11775958c2ec0759a3e1dd84d931233403da044a
nickfrosty <nfrostbutter@gmail.com>
2024-02-04 14:26:56 -0500

    feat: global error boundary

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 #!/usr/bin/env node
 
+import { errorOutro } from "./lib/cli.js";
 import cliProgram from "./commands/index.js";
 import treeCommand from "./commands/tree.js";
 
 async function main() {
-  // display a spacer at the top
-  console.log();
+  try {
+    // display a spacer at the top
+    console.log();
 
-  const program = cliProgram();
+    const program = cliProgram();
 
-  program.addCommand(treeCommand());
+    program.addCommand(treeCommand());
 
-  await program.parseAsync();
+    await program.parseAsync();
 
-  // display a spacer at the bottom
-  console.log();
+    // display a spacer at the bottom
+    console.log();
+  } catch (err) {
+    errorOutro("An internal error occurred");
+  }
 }
 
 main();
